ac13ca4ddf54c9484512aa23b4e296d2d8fa640b
Paolo Genissel-Monsallier <paolo.genissel-monsallier@ubisoft.com>
2019-09-09 21:23:01 +0200

    Added Kubernetes part

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=pgenisselmonsallier/project-ml-microservice-kubernetes
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run project-ml-microservice-kubernetes --image=$dockerpath:latest --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl expose deployment/project-ml-microservice-kubernetes --type="LoadBalancer" --port=8000 --target-port=80
